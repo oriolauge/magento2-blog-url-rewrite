349a139b24cbb6d628ab97716f7ebf24037bac28
Oriol Augé <oriolauge@gmail.com>
2022-07-17 15:32:50 +0200

    Initial commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * OAG Blog url rewrite module registration
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'OAG_BlogUrlRewrite',
+    __DIR__
+);
