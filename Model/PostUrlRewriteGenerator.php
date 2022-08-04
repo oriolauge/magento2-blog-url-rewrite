80b743d95ebb97040568e6705f126779407447fc
Oriol Augé <oriolauge@gmail.com>
2022-08-04 10:44:38 +0200

    Fix generate urls with specific store views values

--- Model/PostUrlRewriteGenerator.php
+++ Model/PostUrlRewriteGenerator.php
@@ -9,6 +9,9 @@ use OAG\BlogUrlRewrite\Model\PostUrlPathGenerator;
 use Magento\UrlRewrite\Service\V1\Data\UrlRewriteFactory;
 use Magento\Store\Model\Store;
 use OAG\Blog\Api\Data\PostInterface;
+use OAG\Blog\Model\PostRepository;
+use OAG\BlogUrlRewrite\Model\PostCheckOverriddenAttributePerStore;
+use OAG\Blog\Setup\PostSetup;
 
 class PostUrlRewriteGenerator
 {
@@ -28,76 +31,107 @@ class PostUrlRewriteGenerator
     protected $postUrlPathGenerator;
 
     /**
-     * Store manager
-     *
      * @var StoreManagerInterface
      */
     protected $storeManager;
 
     /**
-     * @var PostInterface
+     * @var PostRepository
      */
-    protected $post;
+    protected $postRepository;
+
+    /**
+     * @var PostCheckOverriddenAttributePerStore
+     */
+    protected $postCheckOverriddenAttributePerStore;
 
     /**
      * @param UrlRewriteFactory $urlRewriteFactory
      * @param PostUrlPathGenerator $postUrlPathGenerator
      * @param StoreManagerInterface $storeManager
+     * @param PostRepository $postRepository
+     * @param PostCheckOverriddenAttributePerStore $postCheckOverriddenAttributePerStore
      */
     public function __construct(
         UrlRewriteFactory $urlRewriteFactory,
         PostUrlPathGenerator $postUrlPathGenerator,
-        StoreManagerInterface $storeManager
+        StoreManagerInterface $storeManager,
+        PostRepository $postRepository,
+        PostCheckOverriddenAttributePerStore $postCheckOverriddenAttributePerStore
     ) {
         $this->urlRewriteFactory = $urlRewriteFactory;
         $this->storeManager = $storeManager;
         $this->postUrlPathGenerator = $postUrlPathGenerator;
+        $this->postRepository = $postRepository;
+        $this->postCheckOverriddenAttributePerStore = $postCheckOverriddenAttributePerStore;
     }
 
     /**
      * @param PostInterface $post
      * @return \Magento\UrlRewrite\Service\V1\Data\UrlRewrite[]
      */
-    public function generate($post)
+    public function generate(PostInterface $post)
     {
-        $stores = $post->getStores();
-        $this->post = $post;
-        $urls = array_search(Store::DEFAULT_STORE_ID, $stores) === false ? $this->generateForSpecificStores($stores)
-            : $this->generateForAllStores();
-        $this->post = null;
+        $storeId = $post->getStoreId();
+        $urls = $this->isGlobalScope($storeId) ? $this->generateForAllStores($post)
+            : $this->generateForSpecificStoreView($post, $storeId);
         return $urls;
     }
 
     /**
      * Generate list of urls for default store
      *
+     * @param PostInterface $post
      * @return \Magento\UrlRewrite\Service\V1\Data\UrlRewrite[]
      */
-    protected function generateForAllStores()
+    protected function generateForAllStores(PostInterface $post)
     {
         $urls = [];
         foreach ($this->storeManager->getStores() as $store) {
-            $urls[] = $this->createUrlRewrite($store->getStoreId());
+            if (!$this->isGlobalScope($store->getStoreId())
+                && $this->isOverrideUrlsForStore($store->getStoreId(), $post->getId())) {
+                $postStore = $this->postRepository->getById($post->getId(), $store->getStoreId());
+                $urls[] = $this->createUrlRewrite($postStore, $store->getStoreId());
+            } else {
+                $urls[] = $this->createUrlRewrite($post, $store->getStoreId());
+            }
         }
         return $urls;
+    }
+
+
+    /**
+     * Check if we have an specific value for a store id
+     *
+     * @param int $storeId
+     * @param int $postId
+     * @return boolean
+     */
+    protected function isOverrideUrlsForStore($storeId, $postId): bool
+    {
+        if (!$postId) {
+            return false;
+        }
+
+        return $this->postCheckOverriddenAttributePerStore
+            ->doesEntityHaveOverriddenUrlKeyForStore(
+                $storeId,
+                $postId,
+                PostSetup::ENTITY_TYPE_CODE
+            );
+
     }
 
     /**
      * Generate list of urls per store
      *
-     * @param int[] $storeIds
+     * @param PostInterface $post
+     * @param int $storeId
      * @return \Magento\UrlRewrite\Service\V1\Data\UrlRewrite[]
      */
-    protected function generateForSpecificStores($storeIds)
+    protected function generateForSpecificStoreView(PostInterface $post, $storeId)
     {
-        $urls = [];
-        $existingStores = $this->storeManager->getStores();
-        foreach ($storeIds as $storeId) {
-            if (!isset($existingStores[$storeId])) {
-                continue;
-            }
-            $urls[] = $this->createUrlRewrite($storeId);
-        }
+        $urls[] = $this->createUrlRewrite($post, $storeId);
         return $urls;
     }
 
@@ -108,18 +142,29 @@ class PostUrlRewriteGenerator
      * @param int $redirectType
      * @return \Magento\UrlRewrite\Service\V1\Data\UrlRewrite
      */
-    protected function createUrlRewrite($storeId, $redirectType = 0)
+    protected function createUrlRewrite(PostInterface $post, $storeId, $redirectType = 0)
     {
         return $this->urlRewriteFactory->create()->setStoreId($storeId)
             ->setEntityType(self::ENTITY_TYPE)
-            ->setEntityId($this->post->getId())
+            ->setEntityId($post->getId())
             ->setRequestPath(
                 $this->postUrlPathGenerator->getUrlPathWithSuffixAndBlogRoute(
-                    $this->post,
+                    $post,
                     $storeId
                 )
-            )->setTargetPath($this->postUrlPathGenerator->getCanonicalUrlPath($this->post))
+            )->setTargetPath($this->postUrlPathGenerator->getCanonicalUrlPath($post))
             ->setIsAutogenerated(1)
             ->setRedirectType($redirectType);
+    }
+
+    /**
+     * Check is global scope
+     *
+     * @param int|null $storeId
+     * @return bool
+     */
+    protected function isGlobalScope($storeId): bool
+    {
+        return null === $storeId || $storeId == Store::DEFAULT_STORE_ID;
     }
 }
